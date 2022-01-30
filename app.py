09bd81db5fe54b2285caac9d9d727145300b1b59
pendyalaabhi <68814298+pendyalaabhi@users.noreply.github.com>
2022-01-30 21:45:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,46 @@
-from flask import Flask, render_template, request
-import jsonify
-import requests
-import pickle
 import numpy as np
-import sklearn
+from flask import Flask, request, jsonify, render_template, url_for
+import pickle
 from sklearn.preprocessing import StandardScaler
+
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+standard_x = StandardScaler()
 model = pickle.load(open('gradient_boost_regression_model.pkl', 'rb'))
-@app.route('/',methods=['GET'])
-def Home():
+
+
+@app.route('/')
+def home():
     return render_template('index.html')
 
 
-standard_to = StandardScaler()
-@app.route("/predict", methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    Fuel_Type_Diesel=0
-    if request.method == 'POST':
-        Year = int(request.form['Year'])
-        Present_Price=float(request.form['Present_Price'])
-        Kms_Driven=int(request.form['Kms_Driven'])
-        Kms_Driven2=np.log(Kms_Driven)
-        Owner=int(request.form['Owner'])
-        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
-        if(Fuel_Type_Petrol=='Petrol'):
-                Fuel_Type_Petrol=1
-                Fuel_Type_Diesel=0
-        elif(Fuel_Type_Petrol=='Diesel'):
-            Fuel_Type_Petrol=0
-            Fuel_Type_Diesel=1
-        else:
-            Fuel_Type_Petrol=0
-            Fuel_Type_Diesel=0
-        Year=2020-Year
-        Seller_Type_Individual=request.form['Seller_Type_Individual']
-        if(Seller_Type_Individual=='Individual'):
-            Seller_Type_Individual=1
-        else:
-            Seller_Type_Individual=0	
-        Transmission_Mannual=request.form['Transmission_Mannual']
-        if(Transmission_Mannual=='Mannual'):
-            Transmission_Mannual=1
-        else:
-            Transmission_Mannual=0
-        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
-        output=round(prediction[0],2)
-        if output<0:
-            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
-        else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
-    else:
-        return render_template('index.html')
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    standard_value = standard_x.fit_transform(final_features)
+    prediction = model.predict(standard_x.transform(standard_value))
 
-if __name__=="__main__":
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Estimated House Price should be $ {}'.format(output))
+
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls through request
+    '''
+    data = request.get_json(force=True)
+    standard_value = standard_x.fit_transform([np.array(list(data.values()))])
+    prediction = model.predict(standard_x.transform(standard_value))
+
+    output = prediction[0]
+    return jsonify(output)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
